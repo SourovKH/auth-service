d18393f6abf748ef14b17775bb88024c912f1305
Atindra Ghosh <atindraghosh2017@gmail.com>
2024-10-12 16:07:15 +0530

    Connect mongo db

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+const connectDB = require('./src/config/database');
+
 require('dotenv').config();
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-const PORT = process.env.PORT || 8000;
+connectDB();
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log('App listening on port', PORT);    
+    console.log(`Server running on port ${PORT}`);
 });
